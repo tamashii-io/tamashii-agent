ad8df012036027763a7fb57850c2a097559c3037
Liang-Chi Tseng <lctseng@5xruby.tw>
2016-12-28 10:35:25 +0000

    improve fake card reader

--- lib/codeme/agent/device/fake_card_reader.rb
+++ lib/codeme/agent/device/fake_card_reader.rb
@@ -7,12 +7,25 @@ module Codeme
 
         def initialize(*args)
           logger.debug "Initialized"
+          @last_time = Time.now
         end
 
         def picc_request(*args)
+          if Time.now - @last_time > 2
+            @last_time = Time.now
+            if rand > 0.5
+              logger.debug "Fake Card Generated"
+              return true
+            else
+              return false
+            end
+          else
+            return false
+          end
         end
 
         def picc_select(*args)
+          [Array.new(4){ rand(256)}, "sak"]
         end
 
         def picc_halt(*args)
